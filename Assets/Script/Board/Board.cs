5123866e0e32d5888d82c3c1b7655d0eddc71ef3
Eduardo Brandao <dudubrand@gmail.com>
2024-02-06 20:11:48 -0300

    Added board script to handle with board business logic.
    Need investigation about the reason the board creation through Board script is bugging the DotTween

---
+++ Assets/Script/Board/Board.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board
+{
+    private TileData[] _tilesData;
+    private int _tileCount;
+
+    public List<List<Tile>> BoardTiles { get; private set; }
+
+    public Board(int width, int height, TileData[] tilesData)
+    {
+        _tilesData = tilesData;
+        BoardTiles = InitializeBoard(width, height);
+        SetupBoard(BoardTiles);
+    }
+    public List<List<Tile>> CopyBoardTiles(bool self = true, List<List<Tile>> boardTilesToCopy = null)
+    {
+        return self ? 
+            BoardTiles.ConvertAll(row => row.ConvertAll(tile => new Tile().Setup(tile.Data, tile.Id))) :
+            boardTilesToCopy.ConvertAll(row => row.ConvertAll(tile => new Tile().Setup(tile.Data, tile.Id)));
+    }
+    
+    public void SetNewBoardTile(List<List<Tile>> newBoardTiles)
+    {
+        BoardTiles = newBoardTiles;
+    }
+
+    public bool CheckMatches(List<List<Tile>> boardToCheck, int x, int y)
+    {
+        var hasHorizontalMatch = x > 1 && boardToCheck[y][x].Key == boardToCheck[y][x - 1].Key && boardToCheck[y][x - 1].Key == boardToCheck[y][x - 2].Key;
+        var hasVerticalMatch = y > 1 && boardToCheck[y][x].Key == boardToCheck[y - 1][x].Key && boardToCheck[y - 1][x].Key == boardToCheck[y - 2][x].Key;
+        return hasHorizontalMatch || hasVerticalMatch;
+    }
+    
+    public HashSet<Vector2Int> GetMatchesPosition(bool self,List<List<Tile>> boardToCheck = null)
+    {
+        return self ? GetMatchesPosition(BoardTiles) : GetMatchesPosition(boardToCheck);
+    }
+
+    private HashSet<Vector2Int> GetMatchesPosition(List<List<Tile>> boardToCheck = null)
+    {
+        HashSet<Vector2Int> matchesPositions = new HashSet<Vector2Int>();
+        for (int y = 0; y < boardToCheck.Count; y++)
+        {
+            for (int x = 0; x < boardToCheck[y].Count; x++)
+            {
+                if (x > 1 && boardToCheck[y][x].Key == boardToCheck[y][x - 1].Key && boardToCheck[y][x - 1].Key == boardToCheck[y][x - 2].Key)
+                {
+                    matchesPositions.Add(new Vector2Int(x,y));
+                    matchesPositions.Add(new Vector2Int(x-1,y));
+                    matchesPositions.Add(new Vector2Int(x-2,y));
+                }
+                if (y > 1 && boardToCheck[y][x].Key == boardToCheck[y - 1][x].Key && boardToCheck[y - 1][x].Key == boardToCheck[y - 2][x].Key)
+                {
+                    matchesPositions.Add(new Vector2Int(x,y));
+                    matchesPositions.Add(new Vector2Int(x,y-1));
+                    matchesPositions.Add(new Vector2Int(x,y-2));
+                }
+            }
+        }
+        return matchesPositions;
+    }
+    
+    private List<List<Tile>> InitializeBoard(int width, int height)
+    {
+        List<List<Tile>> board = new List<List<Tile>>(height);
+        for (int y = 0; y < height; y++)
+        {
+            board.Add(new List<Tile>(width));
+            for (int x = 0; x < width; x++)
+            {
+                board[y].Add(new Tile());
+            }
+        }
+        return board;
+    }
+    
+    private void SetupBoard(List<List<Tile>> board)
+    {
+        _tileCount = 0;
+        for (int y = 0; y < board.Count; y++)
+        {
+            for (int x = 0; x < board[y].Count; x++)
+            {
+                List<TileData> noMatchTypes = GetAvailableTypes(board,x,y);
+                board[y][x].Setup(noMatchTypes[Random.Range(0, noMatchTypes.Count)],_tileCount++);
+            }
+        }
+    }
+    
+    private List<TileData> GetAvailableTypes(List<List<Tile>> board, int x, int y)
+    {
+        List<TileData> noMatchTypes = new List<TileData>(_tilesData);
+        
+        if (x > 1 && board[y][x - 1].Key == board[y][x - 2].Key)
+        {
+            noMatchTypes.Remove(board[y][x - 1].Data);
+        }
+                
+        if (y > 1 && board[y - 1][x].Key == board[y - 2][x].Key)
+        {
+            noMatchTypes.Remove(board[y - 1][x].Data);
+        }
+        return noMatchTypes;
+    }
+}
