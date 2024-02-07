32547edfd5a1c4d64ed745a19a21548f1b976df9
Eduardo Brandao <dudubrand@gmail.com>
2024-02-07 10:37:21 -0300

    Project folders has been organized

---
+++ Assets/Script/Tile/Tile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Tile
+{
+    private TileData _data;
+    public TileData Data => _data;
+    public string Key => _data.Key;
+    public Color ViewAsset => _data.Type;
+    public int Id { get; private set; } = -1;
+
+    public Tile Setup(TileData data, int id)
+    {
+        _data = data;
+        Id = id;
+        return this;
+    }
+}
